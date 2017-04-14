c82b574b0388dd1444d04eb56c655df8953d6373
MJ Perez <blueavenger@gmail.com>
2017-04-14 12:22:20 -0700

    Adding in fun random name generator

--- src/components/DeckPicker.js
+++ src/components/DeckPicker.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { getFunName } from '../helpers/helper';
 
 class DeckPicker extends React.Component {
   render() {
     return (
       <form className="deck-selector">
         <h3>Please Name your Deck:</h3>
-        <input type="text" required placeholder='Deck Name'/>
+        <input type="text" required placeholder='Deck Name' defaultValue={getFunName()}/>
         <button type="submit"> Start Brewing</button>
       </form>
     )
