f0e850387f661979976c05986267944c9868803f
MJ Perez <blueavenger@gmail.com>
2017-05-22 16:50:18 -0700

    Passing a more detailed state to previewContainer, need to fully realize the impact of the changes

--- src/containers/PreviewContainer.js
+++ src/containers/PreviewContainer.js
@@ -3,9 +3,18 @@ import { connect } from 'react-redux';
 import { addCard } from '../actions/index'
 
 const mapStateToProps = (state) => {
-  return {
-    state
-  }
+  const { deck } = state
+  // Currently converting the deck array into an object to populate
+  // in that props of Preview. We need to start working with
+  // semi-real data and make sure we are modeling/massaging the correct
+  // architechture that scales best
+  const deckObj = deck.reduce((acc, cur, i) => {
+    const id = cur.id
+    acc[id] = cur;
+    return acc;
+  }, {});
+  console.log('deckObj: ', deckObj)
+  return deckObj
 }
 
 const mapDispatchToProps = (dispatch) => {
