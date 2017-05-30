fdbec640120b986c7dd93f62c25526e5050add98
MJ Perez <blueavenger@gmail.com>
2017-05-29 18:19:15 -0700

    Adding export function for decks

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -3,7 +3,13 @@ import PreviewCard from './PreviewCard';
 import PropTypes from 'prop-types';
 
 class Preview extends React.Component {
-  props = this.props;
+  exportDeck() {
+    const deck = this.props.deck;
+    for (const card in deck) {
+      console.log(`${deck[card].count} ${deck[card].name}`);
+    }
+  }
+
   render() {
     const currentDeck = this.props.deck
     const previewCards = Object.keys(currentDeck).map((multiverseid) => <PreviewCard {...currentDeck[multiverseid]} onCardClick={this.props.onCardClick} key={multiverseid}></PreviewCard>);
@@ -11,6 +17,7 @@ class Preview extends React.Component {
     return (
       <div className="preview-view-item">
         <p>Deck Preview</p>
+        <span onClick={() => this.exportDeck()}>Export</span>
         <div className="card-preview-container">
           { previewCards }
         </div>
